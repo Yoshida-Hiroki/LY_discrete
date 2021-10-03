a5cf79385c1cb7fc89d33d310718b143cb2b025f
Yoshi yoshi <hiroki.3141592@gmail.com>
2021-10-03 16:38:16 +0900

    1003 ver4 p1=p2=0.5 T big iter = 10

--- static/stat_2level_dT.py
+++ static/stat_2level_dT.py
@@ -8,13 +8,13 @@ import cmath
 
 type = r"\discrete2dT"
 date = "211003"
-ver = "3"
+ver = "4"
 
 z = Symbol('z')
 
 # initial distrib
-p1 = 0.8
-p2 = 0.2
+p1 = 0.5
+p2 = 0.5
 
 # transition matrix elements
 a_R = 0.4

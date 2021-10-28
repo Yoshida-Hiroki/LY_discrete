8c4e43d073e10dd4bfa49b6506494f965116b5ed
Yoshi yoshi <hiroki.3141592@gmail.com>
2021-10-28 20:16:45 +0900

    ver7 big fluctuation, overlapping

--- time-dependent/timedep_2level_simulation.cpp
+++ time-dependent/timedep_2level_simulation.cpp
@@ -15,18 +15,18 @@ int M = 100;
 int iter = 100000;
 
 string date = "211028";
-string ver = "_6";
+string ver = "_7";
 
 double r(double x){
   return pi/100*sin(x);
 }
 
 double phi_a(double x){
-  return pi*3/4+pi/50*cos(x);
+  return pi*3/4+pi/10*cos(x);
 }
 
 double phi_b(double x){
-  return pi/4+pi/50*sin(x);
+  return pi/4+pi/10*sin(x);
 }
 
 double a_L(double x){
@@ -104,7 +104,7 @@ int main(){
 
   string path = "C:/Users/hyoshida/Desktop/timedep/";
   string file = ".dat";
-  string filename = path+ "sim"+date+ver + file;
+  string filename = path+ "sim_"+date+ver + file;
   ofstream writing_file;
   writing_file.open(filename, ios::out);
 

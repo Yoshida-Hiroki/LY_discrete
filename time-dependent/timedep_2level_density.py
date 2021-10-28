e06f6be8efc4804974692c7a57421b4feb6938a7
Yoshi yoshi <hiroki.3141592@gmail.com>
2021-10-28 13:24:41 +0900

    ver2 zero plot

--- time-dependent/timedep_2level_density.py
+++ time-dependent/timedep_2level_density.py
@@ -9,11 +9,11 @@ import cmath
 
 type = r"\time_density"
 date = "211028"
-ver = "1"
+ver = "2"
 
 # definitions of functions
 # R = lambda x: np.ones_like(x)*1
-r = lambda x: np.pi/200*np.sin(x)
+r = lambda x: np.pi/50*np.sin(x)
 phi_a = lambda x: np.pi*3/4+np.pi/10*np.cos(x)
 phi_b = lambda x: np.pi/4+np.pi/10*np.cos(x)
 

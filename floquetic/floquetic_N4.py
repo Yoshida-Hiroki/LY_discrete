bbd60559ec753935c44b4d7b815cede49f18421e
Yoshi yoshi <hiroki.3141592@gmail.com>
2021-11-21 17:44:22 +0900

    ver15

--- floquetic/floquetic_N4.py
+++ floquetic/floquetic_N4.py
@@ -10,13 +10,13 @@ import time
 
 type = r"\floquetic_N4_zeros"
 date = "211121"
-ver = "14"
+ver = "15"
 
 z = Symbol('z')
 
 # transition matrix elements
 r_base = 0.5
-r_coef = 0.49
+r_coef = 0.499
 phi_coef = 5
 r = lambda x: r_base+r_coef*np.sin(x)
 phi_a = lambda x: np.pi*0.5+np.pi/phi_coef*np.cos(x)
